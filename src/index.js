fa782b70698a52b99489a1137f7354fc271d052a
praveen24sriv <praveen24.srivastav@gmail.com>
2026-01-06 13:57:42 +0530

    method 2 (better modular way) of connecting to DB

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
-import mongoose from "mongoose";
-import {DB_NAME} from "./constants";
-import express from "express"
-const app = express()
+//require('dotenv').config({path:'/.env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
 
-//IIFE
-(async ()=>{
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}`/`${DB_NAME}`)
 
-        app.on("error",(error)=>{
-            console.log("ERROR :",error);
-            throw error
-        })
+dotenv.config({
+    path:'/.env'
+})
 
-        app.listen(process.env.PORT,()=>{
-            console.log(`App is listening at ${process.env.PORT}`)
-        })
-    } catch (error) {
-        console.error("ERROR :" , error);
-        throw error
-    }
-
-})()
+connectDB();
