e2598eb3e81c8aa7309ec0d7ff5c3ba24a4983fc
praveen24sriv <praveen24.srivastav@gmail.com>
2026-01-06 13:24:00 +0530

    method 1 of connecting db - everything in index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants";
+import express from "express"
+const app = express()
+
+//IIFE
+(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}`/`${DB_NAME}`)
+
+        app.on("error",(error)=>{
+            console.log("ERROR :",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening at ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("ERROR :" , error);
+        throw error
+    }
+
+})()
