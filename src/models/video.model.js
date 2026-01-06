695e1dd271c29edc8004e790c3622518933056bb
praveen24sriv <praveen24.srivastav@gmail.com>
2026-01-06 21:04:32 +0530

    video model

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -37,8 +37,7 @@ const videoSchema = new mongoose.Schema(
         }
     }
     ,{timestamps:true});
-    
+
 videoSchema.plugin(mongooseAggregatePaginate);
 export const Video = mongoose.model("Video", videoSchema);
-
 
