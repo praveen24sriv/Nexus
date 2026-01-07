52bdcd2d2826ceb36d884e65b5695de4359d5f76
praveen24sriv <praveen24.srivastav@gmail.com>
2026-01-07 17:18:06 +0530

    register controller steps

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,9 +2,15 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 
 const registerUser = asyncHandler( async (req, res)=>{
-    res.status(200).json({
-        message :"hora h hora h dhire dhire hora h"
-    })
+    //get user details from frontend
+    //validation
+    //check if user already exists
+    //check for file , images , avatar
+    //upload it to cloudinary
+    //create user object - create entry in DB
+    //remove password and refreshtoken from response
+    //check for user creation
+    //return response
 })
 
 export {registerUser}
