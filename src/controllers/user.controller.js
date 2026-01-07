bf0c5b841c79766d7141f0e9e887c1e717ed1e54
praveen24sriv <praveen24.srivastav@gmail.com>
2026-01-07 23:56:16 +0530

    login user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,22 @@ import {User} from "../models/user.model.js" // from mongoose
 import { uploadOnCloudinary } from "../utils/Cloudinary.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 
+const generateAccessAndRefreshTokens= async(userId)=>{
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken=refreshToken
+        await user.save({validateBeforeSave:false})
+        
+        return{accessToken,refreshToken}
+
+    } catch (error) {
+        throw new ApiError(500,"something went wrong while generating access and refresh token")
+    }
+}
+
 
 const registerUser = asyncHandler( async (req, res)=>{
     /*get user details from frontend
@@ -75,4 +91,69 @@ const registerUser = asyncHandler( async (req, res)=>{
 
 })
 
-export {registerUser}
+const loginUser = asyncHandler( async (req,res)=>{
+
+    /* 1)req body - > data
+       2)username or email
+       3)find the user 
+       4)password
+       5)access and refresh token
+       6)send cookie.  */
+
+
+    const {email,username,password} = req.body//1st step
+
+
+    if(!username || !email){                       //2nd step
+        throw new ApiError(400,"username or email is required")
+    }
+
+    const user = await User.findOne({
+        $or:[{username},{email}]//3rd step
+    })
+    if(!user){
+        throw new ApiError(404,"User does not exist")
+    }
+
+
+    const isPasswordValid= await user.isPasswordCorrect(password) //4th step
+     if(!isPasswordValid){
+        throw new ApiError(401,"Password is not correct")
+    }
+
+
+    const { accessToken,refreshToken }=await //5th step
+    generateAccessAndRefreshTokens(user._id)
+
+    
+    const loggedInUser= await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    
+    const options ={  //option->object
+        httpOnly:true,
+        secure:true
+    }
+    return res
+    .status(200)
+    .cookie("accessToken",accessToken,options) //6th step
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponse(
+            200,{
+                user:loggedInUser,accessToken,refreshToken //again sending in case user
+                                                        // wants to save access and refresh token
+            },
+            "User logged in successfully"   
+        )
+    )
+
+})
+
+
+
+
+export {
+    registerUser,
+    loginUser
+}
