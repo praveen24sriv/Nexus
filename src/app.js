a5e49e2a0661f8f3de7a50373291b875b6fa6491
praveen24sriv <praveen24.srivastav@gmail.com>
2026-01-07 16:19:44 +0530

    adding user.routes and fixing app.js and controller

--- src/app.js
+++ src/app.js
@@ -4,22 +4,10 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.use(cors({ //for cross origin requests
-        origin:process.env.CORS_ORIGIN ,
-        credentials:true
-}));
-
-app.use(express.json({// for json requests
-    limit: '16kb'
-}));
-
-app.use(express.urlencoded({ // for url requests
-    extended: true,
-    limit: '16kb'
-}));
-
+app.use(cors({ origin:process.env.CORS_ORIGIN ,credentials:true}));//for cross origin requests
+app.use(express.json({limit: '16kb'}));// for json requests
+app.use(express.urlencoded({ extended: true,limit: '16kb'}));// for url requests
 app.use(express.static('public')); // for static files
-
 app.use(cookieParser()); // for parsing cookies
 
 export default app;
