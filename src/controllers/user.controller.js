e4c0c1aa2913a1714020774c6e234317b1e5d738
praveen24sriv <praveen24.srivastav@gmail.com>
2026-01-09 13:15:51 +0530

    channel profile (aggregation pipelines)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -320,6 +320,85 @@ const updateUserCoverImage=asyncHandler(async(req,res)=>{
     .json( new ApiResponse(200,user,"cover image updated successfully"))
 })
 
+const getUserChannelProfile= asyncHandler(async(req,res)=>{
+    const {username} = req.params
+    if(!username?.trim()){
+        throw new ApiError(400,"username not found")
+    }
+    //User.find({username}) //can do 
+
+    const channel= await User.aggregate([
+        {
+            $match:{
+                username:username?.toLowerCase() //1st pipleine
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",    //as Subscription --> subscriptions in db
+                localField: "_id",
+                foreignField:"channel",   //2nd pipeline
+                as:"subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",    //as Subscription --> subscriptions in db
+                localField: "_id",
+                foreignField:"subscriber",   //2nd pipeline
+                as:"subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+                subscribersCount:{
+                    $size:"$subscribers"
+                },
+                channelsSubscribedToCount:{
+                    $size:"$subscribedTo"
+                },
+                isSubscribed :{
+                    $cond:{
+                        if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+                        then:true,
+                        else:false
+                    }
+                }
+            }
+        },
+        {
+            $project:{ //project only selected things (which are set to 1)
+                fullName:1,
+                username:1,
+                subscribersCount:1,
+                channelsSubscribedToCount:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1,
+                email:1
+            }
+        }
+        
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(404,"Channel not found")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        channel[0],
+        "User channel fetched successfully"
+    ))
+
+})
+
+const getWatchHistory = asyncHandler(async(req,res)=>{
+
+})
+
 export {
     registerUser,
     loginUser,
@@ -329,5 +408,7 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
 }
