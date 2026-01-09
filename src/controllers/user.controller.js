428117fb9d3c33d38994495b0e12236674bcf700
praveen24sriv <praveen24.srivastav@gmail.com>
2026-01-09 10:04:54 +0530

    change current password controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -222,10 +222,31 @@ try {
     throw new ApiError(401,error?.message || "Invalid Refresh Token")   
 }
 })
- 
+
+const changeCurrentPassword = asyncHandler(async(req,res)=>{
+     const {oldPassword , newPassword}= req.body
+
+     const user = await User.findById(req.praveen?._id)
+
+     const isPasswordCorrect= await user.isPasswordCorrect(oldPassword)
+     if(!isPasswordCorrect){
+        throw new ApiError(400,"Invalid Old Password")
+     }
+
+     user.password= newPassword
+     await user.save({validateBeforeSave:false})
+    
+    return res
+    .status(200)
+    .json(new ApiResponse(200,{},"Password changed successfully"))
+})
+
+
+
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentPassword
 }
