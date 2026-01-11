e5aea8220088e6b13d60766d5a458a19ffa8636b
praveen24sriv <praveen24.srivastav@gmail.com>
2026-01-11 14:23:18 +0530

    dashboard controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -10,6 +10,8 @@ const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
 })
 
+
+
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
 })
